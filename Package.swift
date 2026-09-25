699d7ce73c75167b1d76dc563a89b25c21b63ae4
XMediator CI <vision-team@x3mads.com>
2026-09-25 20:38:43 +0000

    Release 5.11.1

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@ import PackageDescription
 let package = Package(
     name: "XMediatorLevelPlayMyTargetAdapterCompatibility",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v14),
     ],
     products: [
         .library(name: "LevelPlayMyTargetAdapter", targets: ["LevelPlayMyTargetAdapterWrapper"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ironsource-mobile/LevelPlay-MyTarget-Adapter-Swift-Package.git", exact: "5.13.0"),
+        .package(url: "https://github.com/ironsource-mobile/LevelPlay-MyTarget-Adapter-Swift-Package.git", exact: "5.11.1"),
     ],
     targets: [
         .target(
--- README.md
+++ README.md
@@ -18,7 +18,7 @@ coexist.
 Add this repository as a package dependency and select the
 `LevelPlayMyTargetAdapter` product instead of LevelPlay's `MyTargetAdapter`.
 Versions match the LevelPlay adapter versions one-to-one: version
-`5.13.0` of this package uses LevelPlay adapter `5.13.0`.
+`5.11.1` of this package uses LevelPlay adapter `5.11.1`.
 
 ## License
 
